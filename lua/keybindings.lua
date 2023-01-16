d5c287b892386484d9a805cc2a3b4e23bb8f39f2
Spencer Gilson <spencer.gilson@gmail.com>
2023-01-16 13:14:57 -0800

    Fix NvimTree shortcut

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,7 +1,7 @@
 local map = vim.api.nvim_set_keymap
 vim.g.mapleader=' '
 
-map('n', 'n', [[:NvimTreeFocus<CR>]], {})
+map('n', '<leader>n', [[:NvimTreeFocus<CR>]], {})
 map('n', '<leader>t', [[:vsplit | terminal<CR>]], {})
 map('n', '<leader>q', [[:q<CR>]], {})
 
